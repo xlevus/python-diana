f14b6d046027105024dcb99669498d7463028024
Chris Targett <chris@xlevus.net>
2017-06-19 21:23:37 +0100

    Version bump to test travis deploys to pypi

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from setuptools import setup
 
 setup(
     name='diana',
-    version='0.0.1',
+    version='0.0.2',
     description='Simple Dependency Injector for python',
     long_description='',
     author='Chris Targett',
     author_email='chris@xlevus.net',
     url='http://github.com/xlevus/diana',
     packages=['diana'],
+    tests_require=['pytest', 'mock'],
 )

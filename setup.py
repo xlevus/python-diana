99ac25224ad136e679dbc15a879f7d128934edc3
Chris Targett <chris@xlevus.net>
2017-12-20 12:54:50 +0000

    Remvoed python3.4 classifier

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'],
 )

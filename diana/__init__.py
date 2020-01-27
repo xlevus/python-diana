42b496b7424cf463a871a71db6ab7d878f276d73
Chris Targett <chris@xlevus.net>
2020-01-27 13:05:09 +0000

    v3.1.1

--- diana/__init__.py
+++ diana/__init__.py
@@ -1,6 +1,6 @@
 from .injector import Injector, NoProvider  # noqa
 from .module import Module, provider, contextprovider, provides  # noqa
 
-__version__ = "3.1.0"
+__version__ = "3.1.1"
 
 injector = Injector()
